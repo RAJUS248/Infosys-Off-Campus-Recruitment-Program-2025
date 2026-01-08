89d548f12dc5a3d221dec4433e58a085d524d82d
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2026-01-08 08:42:58 +0530

    Add files via upload

---
+++ 81_Jump Game.py
@@ -0,0 +1,15 @@
+def canJump(nums):
+
+    max_reach = 0
+
+    for i in range(len(nums)):
+
+        if i > max_reach:
+            return False
+        
+        max_reach = max(max_reach, i + nums[i])
+
+    return True
+
+nums = [2,1,0,2,2,0,3]
+print(canJump(nums))
